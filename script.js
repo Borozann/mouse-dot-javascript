6aef0bc8b31aa997b3398d8b77ca39ac060da6e4
Borozann <gabriel.borozan83@gmail.com>
2025-11-12 12:38:26 -0300

    Removing useless code

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const block = document.querySelector(".block");
 
-const normalSize = function () {
-  block.style.height = 30 + "px";
-  block.style.width = 30 + "px";
-};
-
-const bigSize = function () {
-  block.style.height = 40 + "px";
-  block.style.width = 40 + "px";
-};
-
 document.addEventListener("mousemove", function (e) {
   setTimeout(() => {
     block.style.left = e.clientX - 20 + "px";
