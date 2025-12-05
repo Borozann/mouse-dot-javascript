5197b0b372fcdb150353ec3287b50298a365593f
Borozann <gabriel.borozan83@gmail.com>
2025-12-05 15:06:49 -0300

    Refactor: Utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,10 +1,10 @@
-export const newCounterInc = function (max) {
+export const newCounterInc = function (max = 10) {
   let counter = 0;
-  return function (inc) {
-    if (counter <= max) {
+  return function (inc = 1) {
+    if (counter + inc <= max) {
       return (counter += inc);
-    } else {
-      return -1;
     }
+
+    return -1;
   };
 };
